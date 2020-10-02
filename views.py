ada5afd48d26b7fe993ddcbbc965ecdcd5678909
ysm2509 <55803612+ysm2509@users.noreply.github.com>
2020-10-02 11:35:00 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from CCS.models import Login
+from CCS.forms import DocumentForm 
 # Create your views here.
-
+def base(request):
+	return render(request,'CCS/base.html')
 def signin(request):
-	#print("hai")
+	
 	if request.method=="POST":
 	       userid=request.POST['userid']
 	       pwd=request.POST['pwd']
 	       data=Login.objects.create(userid=userid,pwd=pwd)
-	       return HttpResponse("success")
-	return render(request,'CCS/signin.html')       
+	       return redirect('uploadhtml')
+	return render(request,"CCS/signin.html")              
 
         
-	
+def uploadhtml(request):
+	if request.method == 'POST':
+		form=DocumentForm(request.POST,request.FILES)
+		if form.is_valid():
+			form.save()
+			return redirect('uploadhtml')
+	else :
+		form=DocumentForm()
+	return render(request,'CCS/uploadhtml.html',{'form':form})
+	     		
