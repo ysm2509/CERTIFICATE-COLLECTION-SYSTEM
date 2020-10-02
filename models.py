9b70125e9045e53f25d7f7f33c10f3edc77bb603
ysm2509 <55803612+ysm2509@users.noreply.github.com>
2020-10-02 11:34:29 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -6,5 +6,12 @@ class Login(models.Model):
      userid= models.CharField(max_length=50,null=True)
      pwd=models.CharField(max_length=8,null=True)
 
-     def __str__(self):
-     	return self.userid
+#     def __str__(self):
+#     	return self.userid
+
+
+#class Upload(models.Model):
+class Document(models.Model):
+    description = models.CharField(max_length=255, blank=True)
+    document = models.FileField(upload_to='documents/')
+    uploaded_at = models.DateTimeField(auto_now_add=True) 			     	
