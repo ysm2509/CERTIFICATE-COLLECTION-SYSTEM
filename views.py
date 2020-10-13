369e8748e45d80b2dfd33ad5f2289de94ae9e909
ysm2509 <55803612+ysm2509@users.noreply.github.com>
2020-10-13 19:25:26 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from CCS.models import Login
+from CCS.models import Login,Document
 from CCS.forms import DocumentForm 
+import csv
 # Create your views here.
 def base(request):
 	return render(request,'CCS/base.html')
@@ -20,8 +21,26 @@ def uploadhtml(request):
 		form=DocumentForm(request.POST,request.FILES)
 		if form.is_valid():
 			form.save()
-			return redirect('uploadhtml')
+			return redirect('showdocument')
 	else :
 		form=DocumentForm()
 	return render(request,'CCS/uploadhtml.html',{'form':form})
-	     		
+
+def exportcsv(request):
+	details=Login.objects.all()
+	response=HttpResponse('text/csv')
+	response['Content-Disposition']='attachment;filename=login.csv'
+	writer=csv.writer(response)
+	writer.writerow(['userid','pwd'])
+	fields=details.values_list('userid','pwd')
+	for i in fields:
+		writer.writerow(i)
+	return response	
+def showlogin(request):
+	logins=Login.objects.all()
+	return render(request,"CCS/showlogin.html",{'login':logins})
+
+def showdocument(request):
+	docs=Document.objects.all()
+	return render(request,"CCS/showdocument.html",{'document':docs})
+
